6b17fe5d3e2ee44af3c3e692de172b3ee372acdf
Yam Gomes <yamgcunha@gmail.com>
2021-07-20 19:57:00 -0300

    pedro consertou tomara que funcione

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const Twitter = require("twitter");
 const { createCanvas, loadImage } = require("canvas");
-const fs = require("fs");
-const myCanvas = createCanvas(1000, 200);
-const ctx = myCanvas.getContext("2d");
 
 var start = new Date(2021, 05, 07);
 var end = new Date(2021, 08, 18);
@@ -11,6 +8,7 @@ var diff = Math.ceil((end.getTime() - now.getTime()) / (1000 * 60 * 60 * 24.0));
 var total = Math.ceil(
   (end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24.0)
 );
+
 var msg = "",
   final = "";
 const adj = [
@@ -71,88 +69,77 @@ const adj = [
   "voltas desse circular inverso que chamamos de terra#",
 ];
 
-function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
-  if (typeof stroke == "undefined") {
-    stroke = true;
-  }
-  if (typeof radius === "undefined") {
-    radius = 5;
-  }
-  ctx.beginPath();
-  ctx.moveTo(x + radius, y);
-  ctx.lineTo(x + width - radius, y);
-  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-  ctx.lineTo(x + width, y + height - radius);
-  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-  ctx.lineTo(x + radius, y + height);
-  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-  ctx.lineTo(x, y + radius);
-  ctx.quadraticCurveTo(x, y, x + radius, y);
-  ctx.closePath();
-  if (stroke) {
-    ctx.stroke();
-  }
-  if (fill) {
-    ctx.fill();
-  }
-}
-
-async function loadLogo(ctx, x, y) {
-  await loadImage("logo.png").then((image) => {
-    ctx.drawImage(image, x - 75, y - 70);
-  });
-}
-
-async function loadingBar(ctx, diff) {
-  await loadImage("barra.png").then((image) => {
-    pat = ctx.createPattern(image, "repeat");
-    ctx.fillStyle = pat;
-    ctx.fillRect(80, 110, 820 - (820.0 * diff) / total, 10);
-  });
-}
-
-async function progressIndicator(ctx, x, y) {
-  //bolinha
-  // ctx.beginPath();
-  // ctx.arc(x, y, 10, 0, 2 * Math.PI, true);
-  // ctx.fill();
-
-  //triangulo
-  ctx.beginPath();
-  ctx.moveTo(x, y - 15);
-  ctx.lineTo(x + 10, y - 25);
-  ctx.lineTo(x - 10, y - 25);
-  ctx.fill();
-
-  //logo
-  await loadLogo(ctx, x, y);
-}
-
-async function draw() {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, 1000, 200);
-
-  // barra de fora
-  ctx.fillStyle = "#c2e1ff";
-  roundRect(ctx, 75, 105, 830, 20, 5, true, true);
-
-  // loadingBarHtml(ctx);
-  await loadingBar(ctx, diff).then(() => {
-    // barra
-    ctx.fillStyle = "#254AA5";
-    progressIndicator(ctx, 80 + 820 - (820.0 * diff) / total, 115).then(() => {
-      const buffer = myCanvas.toBuffer("image/png");
-      fs.writeFileSync("./final.jpg", buffer);
-    });
-  });
-}
-
 var client = new Twitter({
   consumer_key: process.env.consumer_key,
   consumer_secret: process.env.consumer_secret,
   access_token_key: process.env.access_token,
   access_token_secret: process.env.access_token_secret,
 });
+
+async function twitterPostImage(image, text) {
+  client.post(
+    "media/upload",
+    { media: image },
+    function (error, media, response) {
+      if (!error) {
+        // If successful, a media object will be returned.
+        console.log(media);
+
+        // Lets tweet it
+        var status = {
+          status: text,
+          media_ids: media.media_id_string, // Pass the media id string
+        };
+
+        client.post(
+          "statuses/update",
+          status,
+          function (error, tweet, response) {
+            if (!error) {
+              console.log(tweet);
+            }
+          }
+        );
+      } else console.log(error);
+    }
+  );
+}
+
+async function drawProgressBar() {
+  // carregar primeiro as coisas async te poupa de ter 1 milhao de thens
+  const logoUFRN = await loadImage("logo.png");
+  const barrinha = await loadImage("barra.png");
+
+  // agora daqui pra frente é so fazer as operacoes normalmente
+  const canvas = createCanvas(1000, 200); // chutei um tamanho
+  const context = canvas.getContext("2d");
+  // background
+  context.fillStyle = "#fff";
+  context.fillRect(0, 0, 1000, 200);
+  // barra de fora
+  context.fillStyle = "#c2e1ff";
+  roundRect(context, 75, 105, 830, 20, 5, true, true);
+  // loading bar
+  context.fillStyle = context.createPattern(barrinha, "repeat");
+  context.fillRect(80, 110, 820 - (820.0 * diff) / total, 10);
+  // barra?
+  context.fillStyle = "#254AA5";
+  context.beginPath();
+  context.moveTo(900 - (820.0 * diff) / total, 100);
+  context.lineTo(910 - (820.0 * diff) / total, 90);
+  context.lineTo(890 - (820.0 * diff) / total, 90);
+  context.fill();
+  // logo
+  context.drawImage(logoUFRN, 825 - (820.0 * diff) / total, 45);
+  // return image
+  return canvas.toBuffer("image/png");
+}
+
+async function tweetWithImage(message) {
+  const image = await drawProgressBar();
+  await twitterPostImage(image, message);
+  console.log("Imagem enviada");
+}
 
 if (diff >= 0) {
   if (diff == 0) {
@@ -167,39 +154,9 @@ if (diff >= 0) {
     } else {
       final = final.slice(0, -1);
     }
-
     msg = `faltam ${diff} ${final}`;
   }
-
-  draw().then(() => {
-    imageToPost = fs.readFileSync("./final.jpg");
-    client.post(
-      "media/upload",
-      { media: imageToPost },
-      function (error, media, response) {
-        if (!error) {
-          // If successful, a media object will be returned.
-          console.log(media);
-
-          // Lets tweet it
-          var status = {
-            status: msg,
-            media_ids: media.media_id_string, // Pass the media id string
-          };
-
-          client.post(
-            "statuses/update",
-            status,
-            function (error, tweet, response) {
-              if (!error) {
-                console.log(tweet);
-              }
-            }
-          );
-        }
-      }
-    );
-  });
+  tweetWithImage(msg);
 } else {
   msg = `😎🏖`;
   client.post("statuses/update", { status: msg }, (error, tweet, response) => {
